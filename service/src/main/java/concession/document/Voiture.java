480351f31e8b67ea46aa1fa14fdd1f4bb9b4c7e3
Da Silva Mendonca Thomas <thomas.dasilvamendonca@protonmail.com>
2023-05-24 14:56:46 +0200

    add quickstart demo

---
+++ service/src/main/java/concession/document/Voiture.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package concession.document;
+
+import java.util.List;
+
+/**
+ *
+ * @author tdasilvamendonca
+ */
+public class Voiture {
+    private String marqueId;
+    private String immat;
+    private String dateImmat;
+    private double prix;
+    private List<String> type;
+    private Document caracteristiques;
+    private String couleur;
+    
+    
+}
--- service/src/main/java/concession/service/ServiceApplication.java
+++ service/src/main/java/concession/service/ServiceApplication.java
@@ -1,13 +1,32 @@
 package concession.service;
 
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import static com.mongodb.client.model.Filters.eq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
-	}
-
+	public static void main( String[] args ) {
+            
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb://localhost:27017";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("concession");
+            MongoCollection<Document> collection = database.getCollection("voitures");
+            Document doc = collection.find().first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
 }
