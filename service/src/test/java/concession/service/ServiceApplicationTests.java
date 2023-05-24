bb9644e2b5d3abbd202ab994b88e050dfac8e11c
Da Silva Mendonca Thomas <thomas.dasilvamendonca@protonmail.com>
2023-05-24 14:29:58 +0200

    add service

---
+++ service/src/test/java/concession/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package concession.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
